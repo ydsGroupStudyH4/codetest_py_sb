8e4bc2394f22873c616e3bab77011a9916fcc0b5
seongbae15 <pseongbae@gmail.com>
2022-05-01 22:54:48 +0900

    update quiz 32 py

---
+++ lv1_32_get_player.py
@@ -0,0 +1,31 @@
+def solution(participant, completion):
+    answer = ""
+    participant.sort()
+    completion.sort()
+    completion.append("0")
+
+    # dic = {}
+    # temp = 0
+    # for p in participant:
+    #     dic[hash(p)] = p
+    #     temp += int(hash(p))
+
+    # for i in range(0, len(participant)):
+    #     if participant[i] != completion[i]:
+    #         answer = participant[i]
+    #         break
+
+    # for p in participant:
+    #     if p in completion:
+    #         if participant.count(p) != completion.count(p):
+    #             answer = p
+    #             break
+    #     else:
+    #         answer = p
+    #         break
+
+    return answer
+
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
