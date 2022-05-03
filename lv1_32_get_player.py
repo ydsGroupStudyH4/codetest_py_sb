8774c8dd81a0b855b3b79371a3fdd8308ee621c7
seongbae15 <pseongbae@gmail.com>
2022-05-04 00:34:26 +0900

    Update lv1_32_get_player.py

--- lv1_32_get_player.py
+++ lv1_32_get_player.py
@@ -1,19 +1,19 @@
 def solution(participant, completion):
     answer = ""
-    participant.sort()
-    completion.sort()
-    completion.append("0")
+    # participant.sort()
+    # completion.sort()
+    # completion.append("0")
 
-    # dic = {}
-    # temp = 0
-    # for p in participant:
-    #     dic[hash(p)] = p
-    #     temp += int(hash(p))
+    dic = {}
+    temp = 0
+    for p in participant:
+        dic[hash(p)] = p
+        temp += int(hash(p))
 
-    for i in range(0, len(participant)):
-        if participant[i] != completion[i]:
-            answer = participant[i]
-            break
+    # for i in range(0, len(participant)):
+    #     if participant[i] != completion[i]:
+    #         answer = participant[i]
+    #         break
 
     # for p in participant:
     #     if p in completion:
